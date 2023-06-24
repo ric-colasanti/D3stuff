9414fc19e73d4efe50a63ced4de36a90ca8cafb7
dr ric colasanti <ricardo.colasanti@glasgow.ac.uk>
2023-06-24 10:36:02 +0100

    add load module

---
+++ js/csv.js
@@ -0,0 +1,19 @@
+/* This file contains the CSV processing functions */
+
+/** @type {*} */
+var csv = function(){
+    return{
+        printout : function(res){
+            console.log(res.responseText);
+        }
+    }
+}();
+
+
+
+
+
+ 
+
+
+
